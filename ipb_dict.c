dd22cc0cacb6a3370e03848c11a2f5c0e33a7f82
Josep Castro <jcastro@ingeniamc.com>
2019-03-18 12:19:06 +0100

    global ipb dictionary read functions and dicionary node template example

--- ipb_dict.c
+++ ipb_dict.c
@@ -1,34 +1,33 @@
 /**
  * @file dictionary.c
- * @brief This file contains the COCO dictionary management.
+ * @brief This file contains the dictionary access functions.
  *
  * @author  Firmware department
  * @copyright Ingenia Motion Control (c) 2018. All rights reserved.
  */
 
 #include "ipb_dict.h"
-#include "coco_dictionary_registers.h"
 #include "ipb_dict_usr.h"
 
-uint8_t Ipb_DictRead(Ipb_TMsg* pIpbMsg)
+uint8_t Ipb_DictRead(TIpbDictEntry* ptIpbDict, uint16_t u16DictSz, Ipb_TMsg* pIpbMsg)
 {
     uint8_t u8Ret = NO_ERROR;
 
     uint16_t u16Idx;
-    for (u16Idx = 0; u16Idx < sizeof(ptCocoDict); ++u16Idx)
+    for (u16Idx = 0; u16Idx < u16DictSz; ++u16Idx)
     {
-        if (pIpbMsg->u16Addr == ptCocoDict[u16Idx].u16Key)
+        if (pIpbMsg->u16Addr == ptIpbDict[u16Idx].u16Key)
         {
-            if (ptCocoDict[u16Idx].CocoRead != NULL)
+            if (ptIpbDict[u16Idx].IpbRead != NULL)
             {
-                u8Ret = ptCocoDict[u16Idx].CocoRead(pIpbMsg->pu16Data, &pIpbMsg->u16Size);
+                u8Ret = ptIpbDict[u16Idx].IpbRead(pIpbMsg->pu16Data, &pIpbMsg->u16Size);
             }
 
             break;
         }
     }
 
-    if (u16Idx == sizeof(ptCocoDict))
+    if (u16Idx == u16DictSz)
     {
         u8Ret = NOT_SUPPORTED;
     }
@@ -36,25 +35,25 @@ uint8_t Ipb_DictRead(Ipb_TMsg* pIpbMsg)
     return u8Ret;
 }
 
-uint8_t Ipb_DictWrite(Ipb_TMsg* pIpbMsg)
+uint8_t Ipb_DictWrite(TIpbDictEntry* ptIpbDict, uint16_t u16DictSz, Ipb_TMsg* pIpbMsg)
 {
     uint8_t u8Ret = NO_ERROR;
 
     uint16_t u16Idx;
-    for (u16Idx = 0; u16Idx < sizeof(ptCocoDict); ++u16Idx)
+    for (u16Idx = 0; u16Idx < u16DictSz; ++u16Idx)
     {
-        if (pIpbMsg->u16Addr == ptCocoDict[u16Idx].u16Key)
+        if (pIpbMsg->u16Addr == ptIpbDict[u16Idx].u16Key)
         {
-            if (ptCocoDict[u16Idx].CocoWrite != NULL)
+            if (ptIpbDict[u16Idx].IpbWrite != NULL)
             {
-                u8Ret = ptCocoDict[u16Idx].CocoWrite(pIpbMsg->pu16Data, &pIpbMsg->u16Size);
+                u8Ret = ptIpbDict[u16Idx].IpbWrite(pIpbMsg->pu16Data, &pIpbMsg->u16Size);
             }
 
             break;
         }
     }
 
-    if (u16Idx == sizeof(ptCocoDict))
+    if (u16Idx == u16DictSz)
     {
         u8Ret = NOT_SUPPORTED;
     }
--- ipb_dict.h
+++ ipb_dict.h
@@ -1,6 +1,6 @@
 /**
  * @file ipb_dict.h
- * @brief Header file for the COCO dictionary.
+ * @brief Header file for the Ipb dictionary.
  *
  * @author  Firmware department
  * @copyright Ingenia Motion Control (c) 2019. All rights reserved.
@@ -19,33 +19,44 @@
 /** Write error */
 #define WRITE_ERROR     (uint8_t)0x02U
 
-typedef struct TCocoDictEntry
+typedef struct TIpbDictEntry
 {
+    /** Dictionary entry key value */
     uint16_t u16Key;
-    uint8_t (*CocoRead)(uint16_t* u16Data, uint16_t* u16Size);
-    uint8_t (*CocoWrite)(uint16_t* u16Data, uint16_t* u16Size);
-} TCocoDictEntry;
+    /** Read function callback */
+    uint8_t (*IpbRead)(uint16_t* u16Data, uint16_t* u16Size);
+    /** Write function callback */
+    uint8_t (*IpbWrite)(uint16_t* u16Data, uint16_t* u16Size);
+} TIpbDictEntry;
 
 /**
  * Function to read the value of a Ipb register
  *
+ * @param[in] ptIpbDict
+ *  Ipb dictionary pointer
+ * @param[in] u16DictSz
+ *  Size in number of elements of the dictionary
  * @param[out] pIpbMsg
  *  Message buffer to store the read register
  *
  * @retval result of the access
  */
 uint8_t
-Ipb_DictRead(Ipb_TMsg* pIpbMsg);
+Ipb_DictRead(TIpbDictEntry* ptIpbDict, uint16_t u16DictSz, Ipb_TMsg* pIpbMsg);
 
 /**
  * Function to write the value of a Ipb register
  *
+ * @param[in] ptIpbDict
+ *  Ipb dictionary pointer
+ * @param[in] u16DictSz
+ *  Size in number of elements of the dictionary
  * @param[in] pIpbMsg
  *  Message buffer containing register value
  *
  * @retval result of the access
  */
 uint8_t
-Ipb_DictWrite(Ipb_TMsg* pIpbMsg);
+Ipb_DictWrite(TIpbDictEntry* ptIpbDict, uint16_t u16DictSz, Ipb_TMsg* pIpbMsg);
 
 #endif /* IPB_DICT_H */
